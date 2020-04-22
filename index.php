7c0629e55e75e4fb41c2e0bd3ac91faea4d2dcea
Nektarios Kalogridis <mail@nektarios.com>
2020-04-22 12:15:16 -0400

    Format deep index.

--- index.php
+++ index.php
@@ -122,7 +122,7 @@ fclose($fp);
                 <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
                     <div class="homeTxt">
                         <h2>Deep Index
-                        <span><?php echo $deepIndex; ?> </span></h2>
+                        <span><?php echo number_format($deepIndex); ?> </span></h2>
                         <h6>Next day predicted value of Dow Jones Industrial Average</h6>
                         <div class="homeGraph">
                             <!-- <img src="assets/images/home-chart.png"> -->
