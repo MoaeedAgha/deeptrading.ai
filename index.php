1377708ee79e7ec4ff7557e58b23bebdac3f3b2e
Nektarios Kalogridis <mail@nektarios.com>
2020-04-22 10:53:43 -0400

    Another small change...

--- index.php
+++ index.php
@@ -107,7 +107,7 @@
 			<div class="row">
 				<div class="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8">
 					<div class="copyTxt">
-						<p>&copy; 2020  DeepTrading.ai,  All Rights Reserved Logo &amp;</p> #Website crafted w/<i class="fa fa-heart"></i> by <a href="https://fullstop360.com/" target="_blank">FullStop</a></p>
+						<p>&copy; 2020  DeepTrading.ai,  All Rights Reserved</p>
 					</div>
 				</div>
 				<div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
